bab5c4a6c36849bc4cdc211c03e03d4c68d35787
Evan Goss <evan.goss@gmail.com>
2017-02-03 16:01:21 -0800

    Layout 6 buttons in 2 nested layouts

--- HelloWorld/app/src/main/java/com/example/evangoss/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/evangoss/helloworld/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.evangoss.helloworld;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onClick(View view) {
-        EditText editText = (EditText) findViewById(R.id.txtName);
-        Toast.makeText(MainActivity.this, "Hello "+editText.getText(), Toast.LENGTH_SHORT).show();
     }
 }
