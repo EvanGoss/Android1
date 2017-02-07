b3e01679465b7b40a7c20f4fe205855b37c8fc0a
Evan Goss <evan@evangoss.com>
2017-02-07 00:10:10 -0800

    Add click handler to 2nd button

--- Events/app/src/main/java/com/example/evangoss/events/MainActivity.java
+++ Events/app/src/main/java/com/example/evangoss/events/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.evangoss.events;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popAlert("Coded Event Handler", "You Clicked Me!");
+            }
+        });
     }
 
     @Override
