698e627b51e2e77cce564d368e8a5f8014d140a5
Evan Goss <eg@Evans-MacBook-Pro.local>
2017-03-04 15:12:53 -0800

    Add dynamic menu item

--- Images/app/src/main/java/com/example/eg/images/MainActivity.java
+++ Images/app/src/main/java/com/example/eg/images/MainActivity.java
@@ -1,34 +1,67 @@
 package com.example.eg.images;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    String currentImage = "playerswing";
+    public static final int MENU_ITEM_ID = 1001;
+    ImageView imageView;
+
+    private void popToast(String text) {
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imagePlayer);
     }
 
-    public void onClick(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
 
-        ImageView imageView = (ImageView) findViewById(R.id.imagePlayer);
-        Button imageButton = (Button) findViewById(R.id.btnShowPlayer);
+        MenuItem item = menu.add(Menu.NONE, MENU_ITEM_ID, 103, "Dynamic");
+        return true;
+    }
 
-        if (currentImage == "playerswing") {
-            imageView.setImageResource(R.drawable.playerpitch);
-            currentImage = "playerpitch";
-            imageButton.setText("Show Hit");
-        } else {
-            imageView.setImageResource(R.drawable.playerswing);
-            currentImage = "playerswing";
-            imageButton.setText("Show Pitch");
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_ITEM_ID:
+                popToast("Dynamic Menu Item");
+                break;
+            case R.id.menu_pitch:
+                imageView.setImageResource(R.drawable.playerpitch);
+                break;
+            case R.id.menu_swing:
+                imageView.setImageResource(R.drawable.playerswing);
+                break;
+            case R.id.menu_flag:
+                try {
+                    InputStream stream = getAssets().open("unitedstates.png");
+                    Drawable drawable = Drawable.createFromStream(stream,null);
+                    imageView.setImageDrawable(drawable);
+                } catch (IOException e) {
+                    popToast("Error in onClick");
+                }
+                break;
         }
+        return true;
     }
+
 }
