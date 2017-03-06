362d3ba747942470b2cf4b50698c4aad1e572217
Evan Goss <eg@Evans-MacBook-Pro.local>
2017-03-05 23:07:18 -0800

    Add navigation to Action Bar

--- Activities/app/src/main/java/com/example/eg/activities/DetailActivity.java
+++ Activities/app/src/main/java/com/example/eg/activities/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.eg.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,6 +11,17 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
     }
 }
